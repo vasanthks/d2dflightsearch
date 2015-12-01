4de4e3be7313f0f7a2173c31f49015755f1fd8ff
vgovilkar <vaibhav.govilkar@orbitz.com>
2015-12-01 15:29:48 -0600

    ui refined

--- public/js/angularstuff.js
+++ public/js/angularstuff.js
@@ -1,5 +1,14 @@
-angular.module('example', ['google.places'])
-        // Setup a basic controller with a scope variable 'place'
-        .controller('MainCtrl', function ($scope) {
-            $scope.place = null;
-        });
+var app = angular.module('example', ['google.places']);
+// Setup a basic controller with a scope variable 'place'
+app.controller('MainCtrl', function ($scope) {
+  $scope.origin = null;
+  $scope.destination = null;
+  $scope.departdate = null;
+
+  $scope.searchresults = null;
+
+  $scope.searchnow = function() {
+
+  };
+
+});
